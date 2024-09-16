17432c83777c20da1261efff6097baf51d8938ec
NickyEnglish2 <ilya.fomin.99.62@gmail.com>
2024-09-16 20:35:21 +0800

    Modified gendiff. added format and arguments

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,7 +6,9 @@ const program = new Command();
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
+  .arguments('<filepath1> <filepath2>')
   .version('0.0.1', '-V, --version', 'output the version number')
-  .helpOption('-h, --help', 'output usage information');
+  .helpOption('-h, --help', 'output usage information')
+  .option('-f, --format [type]', 'output format');
 
 program.parse();
