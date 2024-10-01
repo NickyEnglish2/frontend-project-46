c83d5c140c554c037b388469ec98bd6acde65b75
NickyEnglish2 <ilya.fomin.99.62@gmail.com>
2024-10-01 19:50:19 +0800

    Modified stylish.js

--- src/stylish.js
+++ src/stylish.js
@@ -13,32 +13,36 @@ const formatValue = (value, depth) => {
   return value;
 };
 
-const stylishFormat = (diffData, depth = 1) => {
-  const finalResult = diffData.flatMap((node) => {
-    const currentIndent = indent(depth);
-    const markerIndent = currentIndent.slice(2);
-    const nextDepth = depth + 1;
+const stylishFormat = (diffData) => {
+  const formattingFunction = (data, depth = 1) => {
+    const finalResult = data.flatMap((node) => {
+      const currentIndent = indent(depth);
+      const markerIndent = currentIndent.slice(2);
+      const nextDepth = depth + 1;
 
-    switch (node.type) {
-      case 'nested':
-        return `${currentIndent}${node.key}: {\n${stylishFormat(node.children, nextDepth)}\n${currentIndent}}`;
-      case 'added':
-        return `${markerIndent}+ ${node.key}: ${formatValue(node.value, nextDepth)}`;
-      case 'removed':
-        return `${markerIndent}- ${node.key}: ${formatValue(node.value, nextDepth)}`;
-      case 'changed':
-        return [
-          `${markerIndent}- ${node.key}: ${formatValue(node.oldValue, nextDepth)}`,
-          `${markerIndent}+ ${node.key}: ${formatValue(node.newValue, nextDepth)}`,
-        ];
-      case 'unchanged':
-        return `${currentIndent}${node.key}: ${formatValue(node.value, nextDepth)}`;
-      default:
-        throw new Error(`Unknown type: ${node.type}`);
-    }
-  });
+      switch (node.type) {
+        case 'nested':
+          return `${currentIndent}${node.key}: {\n${formattingFunction(node.children, nextDepth)}\n${currentIndent}}`;
+        case 'added':
+          return `${markerIndent}+ ${node.key}: ${formatValue(node.value, nextDepth)}`;
+        case 'removed':
+          return `${markerIndent}- ${node.key}: ${formatValue(node.value, nextDepth)}`;
+        case 'changed':
+          return [
+            `${markerIndent}- ${node.key}: ${formatValue(node.oldValue, nextDepth)}`,
+            `${markerIndent}+ ${node.key}: ${formatValue(node.newValue, nextDepth)}`,
+          ];
+        case 'unchanged':
+          return `${currentIndent}${node.key}: ${formatValue(node.value, nextDepth)}`;
+        default:
+          throw new Error(`Unknown type: ${node.type}`);
+      }
+    });
 
-  return `{\n${finalResult.join('\n')}\n}`;
+    return finalResult.join('\n');
+  };
+
+  return `{\n${formattingFunction(diffData)}\n}`;
 };
 
 export default stylishFormat;
