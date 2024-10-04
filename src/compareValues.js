37d1ebf21052bc26b3e4be903fa1e01cbb334562
NickyEnglish2 <ilya.fomin.99.62@gmail.com>
2024-10-04 22:23:07 +0800

    Modified compareValues

--- src/compareValues.js
+++ src/compareValues.js
@@ -1,42 +1,36 @@
 import _ from 'lodash';
+import formattingDiff from './formattingDiff.js';
+
+const getTypeOfDiff = (obj1, obj2, key) => {
+  const value1 = obj1[key];
+  const value2 = obj2[key];
+
+  if (!_.has(obj1, key)) {
+    return 'added';
+  }
+  if (!_.has(obj2, key)) {
+    return 'removed';
+  }
+  if (_.isPlainObject(value1) && _.isPlainObject(value2)) {
+    return 'nested';
+  }
+  if (!_.isEqual(value1, value2)) {
+    return 'changed';
+  }
+
+  return 'unchanged';
+};
 
 const compareValues = (obj1, obj2) => {
   const unitedKeys = _.union(_.keys(obj1), _.keys(obj2));
   const sortedKeys = _.sortBy(unitedKeys);
 
-  const diffKeys = sortedKeys.map((key) => {
-    const value1 = obj1[key];
-    const value2 = obj2[key];
-
-    if (_.isPlainObject(value1) && _.isPlainObject(value2)) {
-      return {
-        key,
-        type: 'nested',
-        children: compareValues(value1, value2),
-      };
-    }
-
-    if (!_.has(obj1, key)) {
-      return { key, type: 'added', value: value2 };
-    }
-
-    if (!_.has(obj2, key)) {
-      return { key, type: 'removed', value: value1 };
-    }
-
-    if (!_.isEqual(value1, value2)) {
-      return {
-        key,
-        type: 'changed',
-        oldValue: value1,
-        newValue: value2,
-      };
-    }
-
-    return { key, type: 'unchanged', value: value1 };
+  const comparedKeys = sortedKeys.map((key) => {
+    const diffType = getTypeOfDiff(obj1, obj2, key);
+    return formattingDiff(key, obj1, obj2, diffType);
   });
 
-  return diffKeys;
+  return comparedKeys;
 };
 
 export default compareValues;
