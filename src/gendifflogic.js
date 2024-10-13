7444accd31f687d1b29d88d7c7bc6bedb4f1d026
NickyEnglish2 <ilya.fomin.99.62@gmail.com>
2024-10-13 19:10:41 +0800

    Changed gendifflogic for test

--- src/gendifflogic.js
+++ src/gendifflogic.js
@@ -1,10 +1,22 @@
-import mainParsingLogic from './mainParsingLogic.js';
+import fs from 'fs';
+import path from 'path';
+import parser from './parsers.js';
 import getFormatter from './formatters/getFormatter.js';
 import compareValues from './compareValues.js';
 
+const extractingContent = (file) => {
+  const absolutePath = path.resolve(process.cwd(), file);
+  const fileContent = fs.readFileSync(absolutePath, 'utf-8');
+
+  return fileContent;
+};
+
 const genDiff = (file1, file2, format = 'stylish') => {
-  const parsedFile1 = mainParsingLogic(file1);
-  const parsedFile2 = mainParsingLogic(file2);
+  const content1 = extractingContent(file1);
+  const content2 = extractingContent(file2);
+
+  const parsedFile1 = parser(content1);
+  const parsedFile2 = parser(content2);
 
   const result = compareValues(parsedFile1, parsedFile2);
 
