95955c4b2dbcfe4b8acf83478e433b51c0091275
NickyEnglish2 <ilya.fomin.99.62@gmail.com>
2024-10-16 20:28:35 +0800

    United all compareValues function into one

--- src/compareValues.js
+++ src/compareValues.js
@@ -1,44 +1,48 @@
 import _ from 'lodash';
-import formattingDiff from './formattingDiff.js';
-
-const getTypeOfDiff = (obj1, obj2, key) => {
-  const value1 = obj1[key];
-  const value2 = obj2[key];
-
-  if (!_.has(obj1, key)) {
-    return 'added';
-  }
-  if (!_.has(obj2, key)) {
-    return 'removed';
-  }
-  if (_.isPlainObject(value1) && _.isPlainObject(value2)) {
-    return 'nested';
-  }
-  if (!_.isEqual(value1, value2)) {
-    return 'changed';
-  }
-
-  return 'unchanged';
-};
 
 const compareValues = (obj1, obj2) => {
   const unitedKeys = _.union(_.keys(obj1), _.keys(obj2));
   const sortedKeys = _.sortBy(unitedKeys);
 
-  const comparedKeys = sortedKeys.map((key) => {
-    const diffType = getTypeOfDiff(obj1, obj2, key);
+  return sortedKeys.map((key) => {
+    const value1 = obj1[key];
+    const value2 = obj2[key];
 
-    if (diffType === 'nested') {
-      const value1 = obj1[key];
-      const value2 = obj2[key];
-      const children = compareValues(value1, value2);
-      return { ...formattingDiff(key, obj1, obj2, diffType), children };
+    const diffType = (() => {
+      if (!_.has(obj1, key)) {
+        return 'added';
+      }
+      if (!_.has(obj2, key)) {
+        return 'removed';
+      }
+      if (_.isPlainObject(value1) && _.isPlainObject(value2)) {
+        return 'nested';
+      }
+      if (!_.isEqual(value1, value2)) {
+        return 'changed';
+      }
+      return 'unchanged';
+    })();
+
+    switch (diffType) {
+      case 'nested':
+        return { key, type: 'nested', children: compareValues(value1, value2) };
+      case 'added':
+        return { key, type: 'added', value: value2 };
+      case 'removed':
+        return { key, type: 'removed', value: value1 };
+      case 'changed':
+        return {
+          key,
+          type: 'changed',
+          oldValue: value1,
+          newValue: value2,
+        };
+      case 'unchanged':
+      default:
+        return { key, type: 'unchanged', value: value1 };
     }
-
-    return formattingDiff(key, obj1, obj2, diffType);
   });
-
-  return comparedKeys;
 };
 
 export default compareValues;
