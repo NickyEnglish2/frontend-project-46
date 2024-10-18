77c082f1b9e8eb326e1b9979fa4def736c2a1e98
NickyEnglish2 <ilya.fomin.99.62@gmail.com>
2024-10-18 19:38:36 +0800

    Modified compareValues

--- src/compareValues.js
+++ src/compareValues.js
@@ -8,40 +8,24 @@ const compareValues = (obj1, obj2) => {
     const value1 = obj1[key];
     const value2 = obj2[key];
 
-    const diffType = (() => {
-      if (!_.has(obj1, key)) {
-        return 'added';
-      }
-      if (!_.has(obj2, key)) {
-        return 'removed';
-      }
-      if (_.isPlainObject(value1) && _.isPlainObject(value2)) {
-        return 'nested';
-      }
-      if (!_.isEqual(value1, value2)) {
-        return 'changed';
-      }
-      return 'unchanged';
-    })();
-
-    switch (diffType) {
-      case 'nested':
-        return { key, type: 'nested', children: compareValues(value1, value2) };
-      case 'added':
-        return { key, type: 'added', value: value2 };
-      case 'removed':
-        return { key, type: 'removed', value: value1 };
-      case 'changed':
-        return {
-          key,
-          type: 'changed',
-          oldValue: value1,
-          newValue: value2,
-        };
-      case 'unchanged':
-      default:
-        return { key, type: 'unchanged', value: value1 };
+    if (!_.has(obj1, key)) {
+      return { key, type: 'added', value: value2 };
     }
+    if (!_.has(obj2, key)) {
+      return { key, type: 'removed', value: value1 };
+    }
+    if (_.isPlainObject(value1) && _.isPlainObject(value2)) {
+      return { key, type: 'nested', children: compareValues(value1, value2) };
+    }
+    if (!_.isEqual(value1, value2)) {
+      return {
+        key,
+        type: 'changed',
+        oldValue: value1,
+        newValue: value2,
+      };
+    }
+    return { key, type: 'unchanged', value: value1 };
   });
 };
 
