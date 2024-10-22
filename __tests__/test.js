5fadabe0938186b67d0f0aad817499a426ec1757
NickyEnglish2 <ilya.fomin.99.62@gmail.com>
2024-10-22 23:31:08 +0800

    Modified tests for the absolute path

--- __tests__/test.js
+++ __tests__/test.js
@@ -1,203 +1,37 @@
+import { fileURLToPath } from 'url';
+import path from 'path';
 import genDiff from '../src/gendifflogic.js';
+import { result1, result2, result3 } from '../__fixtures__/results.js';
 
-const result1 = `{
-    common: {
-      + follow: false
-        setting1: Value 1
-      - setting2: 200
-      - setting3: true
-      + setting3: null
-      + setting4: blah blah
-      + setting5: {
-            key5: value5
-        }
-        setting6: {
-            doge: {
-              - wow: 
-              + wow: so much
-            }
-            key: value
-          + ops: vops
-        }
-    }
-    group1: {
-      - baz: bas
-      + baz: bars
-        foo: bar
-      - nest: {
-            key: value
-        }
-      + nest: str
-    }
-  - group2: {
-        abc: 12345
-        deep: {
-            id: 45
-        }
-    }
-  + group3: {
-        deep: {
-            id: {
-                number: 45
-            }
-        }
-        fee: 100500
-    }
-}`;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-const result3 = `Property 'common.follow' was added with value: false
-Property 'common.setting2' was removed
-Property 'common.setting3' was updated. From true to null
-Property 'common.setting4' was added with value: 'blah blah'
-Property 'common.setting5' was added with value: [complex value]
-Property 'common.setting6.doge.wow' was updated. From '' to 'so much'
-Property 'common.setting6.ops' was added with value: 'vops'
-Property 'group1.baz' was updated. From 'bas' to 'bars'
-Property 'group1.nest' was updated. From [complex value] to 'str'
-Property 'group2' was removed
-Property 'group3' was added with value: [complex value]`;
-
-const result4 = `{
-  "common": {
-    "key": "common",
-    "type": "nested",
-    "children": [
-      {
-        "key": "follow",
-        "type": "added",
-        "value": false
-      },
-      {
-        "key": "setting1",
-        "type": "unchanged",
-        "value": "Value 1"
-      },
-      {
-        "key": "setting2",
-        "type": "removed",
-        "value": 200
-      },
-      {
-        "key": "setting3",
-        "type": "changed",
-        "oldValue": true,
-        "newValue": null
-      },
-      {
-        "key": "setting4",
-        "type": "added",
-        "value": "blah blah"
-      },
-      {
-        "key": "setting5",
-        "type": "added",
-        "value": {
-          "key5": "value5"
-        }
-      },
-      {
-        "key": "setting6",
-        "type": "nested",
-        "children": [
-          {
-            "key": "doge",
-            "type": "nested",
-            "children": [
-              {
-                "key": "wow",
-                "type": "changed",
-                "oldValue": "",
-                "newValue": "so much"
-              }
-            ]
-          },
-          {
-            "key": "key",
-            "type": "unchanged",
-            "value": "value"
-          },
-          {
-            "key": "ops",
-            "type": "added",
-            "value": "vops"
-          }
-        ]
-      }
-    ]
-  },
-  "group1": {
-    "key": "group1",
-    "type": "nested",
-    "children": [
-      {
-        "key": "baz",
-        "type": "changed",
-        "oldValue": "bas",
-        "newValue": "bars"
-      },
-      {
-        "key": "foo",
-        "type": "unchanged",
-        "value": "bar"
-      },
-      {
-        "key": "nest",
-        "type": "changed",
-        "oldValue": {
-          "key": "value"
-        },
-        "newValue": "str"
-      }
-    ]
-  },
-  "group2": {
-    "key": "group2",
-    "type": "removed",
-    "value": {
-      "abc": 12345,
-      "deep": {
-        "id": 45
-      }
-    }
-  },
-  "group3": {
-    "key": "group3",
-    "type": "added",
-    "value": {
-      "deep": {
-        "id": {
-          "number": 45
-        }
-      },
-      "fee": 100500
-    }
-  }
-}`;
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
 test('testing parser for non format', () => {
-  expect(() => genDiff('__fixtures__/file5.txt', '__fixtures__/file6.txt')).toThrow(Error('Non supported format of file'));
+  expect(() => genDiff(getFixturePath('file5.txt'), getFixturePath('file6.txt'))).toThrow(Error('Non supported format of file'));
 });
 
 test('testing gendiff courser', () => {
-  expect(genDiff('__fixtures__/file1.json', '__fixtures__/file2.json')).toBe(result1);
+  expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'))).toBe(result1);
 });
 
 test('testing gendiff yaml', () => {
-  expect(genDiff('__fixtures__/file3.yml', '__fixtures__/file4.yml')).toBe(result1);
+  expect(genDiff(getFixturePath('file3.yml'), getFixturePath('file4.yml'))).toBe(result1);
 });
 
 test('testing getformatter', () => {
-  expect(() => genDiff('__fixtures__/file1.json', '__fixtures__/file2.json', 'konobaka')).toThrow(Error('Unknown format: konobaka'));
+  expect(() => genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'), 'konobaka')).toThrow(Error('Unknown format: konobaka'));
 });
 
 test('testing gendiff plain', () => {
-  expect(genDiff('__fixtures__/file1.json', '__fixtures__/file2.json', 'plain')).toBe(result3);
+  expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'), 'plain')).toBe(result2);
 });
 
 test('testing gendiff plain yml', () => {
-  expect(genDiff('__fixtures__/file3.yml', '__fixtures__/file4.yml', 'plain')).toBe(result3);
+  expect(genDiff(getFixturePath('file3.yml'), getFixturePath('file4.yml'), 'plain')).toBe(result2);
 });
 
 test('testing json formatter', () => {
-  expect(genDiff('__fixtures__/file1.json', '__fixtures__/file2.json', 'json')).toBe(result4);
+  expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'), 'json')).toBe(result3);
 });
