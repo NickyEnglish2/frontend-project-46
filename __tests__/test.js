ffc8775169f73cc431c02fc61778c79dfea8f284
NickyEnglish2 <ilya.fomin.99.62@gmail.com>
2024-10-23 20:45:01 +0800

    Modified test files

--- __tests__/test.js
+++ __tests__/test.js
@@ -9,30 +9,22 @@ const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const getContent = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
+test.each([
+  ['file1.json', 'file2.json', 'resultStylish.txt', 'stylish'],
+  ['file3.yml', 'file4.yml', 'resultStylish.txt', 'stylish'],
+  ['file1.json', 'file2.json', 'resultPlain.txt', 'plain'],
+  ['file3.yml', 'file4.yml', 'resultPlain.txt', 'plain'],
+  ['file1.json', 'file2.json', 'resultJson.txt', 'json'],
+])('testing gendiff logic', (file1, file2, resultFile, format) => {
+  const expectedResult = getContent(resultFile);
+  const actualResult = genDiff(getFixturePath(file1), getFixturePath(file2), format);
+  expect(actualResult).toBe(expectedResult);
+});
+
 test('testing parser for non format', () => {
   expect(() => genDiff(getFixturePath('file5.txt'), getFixturePath('file6.txt'))).toThrow(Error('Non supported format of file'));
 });
 
-test('testing gendiff courser', () => {
-  expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'))).toBe(getContent('resultStylish.txt'));
-});
-
-test('testing gendiff yaml', () => {
-  expect(genDiff(getFixturePath('file3.yml'), getFixturePath('file4.yml'))).toBe(getContent('resultStylish.txt'));
-});
-
-test('testing getformatter', () => {
+test('testing getformatter with unknown format', () => {
   expect(() => genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'), 'konobaka')).toThrow(Error('Unknown format: konobaka'));
-});
-
-test('testing gendiff plain', () => {
-  expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'), 'plain')).toBe(getContent('resultPlain.txt'));
-});
-
-test('testing gendiff plain yml', () => {
-  expect(genDiff(getFixturePath('file3.yml'), getFixturePath('file4.yml'), 'plain')).toBe(getContent('resultPlain.txt'));
-});
-
-test('testing json formatter', () => {
-  expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'), 'json')).toBe(getContent('resultJson.txt'));
 });
--- src/formatters/json.js
+++ src/formatters/json.js
@@ -1,29 +1,3 @@
-import _ from 'lodash';
-
-const jsonFormat = (diffData) => {
-  const formatNode = (node) => {
-    switch (node.type) {
-      case 'nested':
-        return {
-          key: node.key,
-          type: node.type,
-          children: _.map(node.children, formatNode),
-        };
-      case 'added':
-      case 'removed':
-      case 'unchanged':
-        return _.pick(node, ['key', 'type', 'value']);
-      default:
-        return _.pick(node, ['key', 'type', 'firstValue', 'secondValue']);
-    }
-  };
-
-  const formattedDiff = _.reduce(diffData, (acc, node) => ({
-    ...acc,
-    [node.key]: formatNode(node),
-  }), {});
-
-  return JSON.stringify(formattedDiff, null, 2);
-};
+const jsonFormat = (diffData) => JSON.stringify(diffData, null, 2);
 
 export default jsonFormat;
