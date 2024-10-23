c85a5f771f4bc77696ef984b0765bca6efb95622
NickyEnglish2 <ilya.fomin.99.62@gmail.com>
2024-10-23 20:17:00 +0800

    Modified tests

--- __tests__/test.js
+++ __tests__/test.js
@@ -1,23 +1,24 @@
 import { fileURLToPath } from 'url';
 import path from 'path';
+import fs from 'fs';
 import genDiff from '../src/gendifflogic.js';
-import { result1, result2, result3 } from '../__fixtures__/results.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const getContent = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
 test('testing parser for non format', () => {
   expect(() => genDiff(getFixturePath('file5.txt'), getFixturePath('file6.txt'))).toThrow(Error('Non supported format of file'));
 });
 
 test('testing gendiff courser', () => {
-  expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'))).toBe(result1);
+  expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'))).toBe(getContent('resultStylish.txt'));
 });
 
 test('testing gendiff yaml', () => {
-  expect(genDiff(getFixturePath('file3.yml'), getFixturePath('file4.yml'))).toBe(result1);
+  expect(genDiff(getFixturePath('file3.yml'), getFixturePath('file4.yml'))).toBe(getContent('resultStylish.txt'));
 });
 
 test('testing getformatter', () => {
@@ -25,13 +26,13 @@ test('testing getformatter', () => {
 });
 
 test('testing gendiff plain', () => {
-  expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'), 'plain')).toBe(result2);
+  expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'), 'plain')).toBe(getContent('resultPlain.txt'));
 });
 
 test('testing gendiff plain yml', () => {
-  expect(genDiff(getFixturePath('file3.yml'), getFixturePath('file4.yml'), 'plain')).toBe(result2);
+  expect(genDiff(getFixturePath('file3.yml'), getFixturePath('file4.yml'), 'plain')).toBe(getContent('resultPlain.txt'));
 });
 
 test('testing json formatter', () => {
-  expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'), 'json')).toBe(result3);
+  expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'), 'json')).toBe(getContent('resultJson.txt'));
 });
